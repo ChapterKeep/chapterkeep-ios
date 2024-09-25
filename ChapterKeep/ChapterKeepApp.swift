6122f7b60e3da183aca73dcf0873060056e33cc1
Seungyun Lee <khora.lee.0@gmail.com>
2024-09-25 19:41:31 +0900

    chore: initialize repository

---
+++ ChapterKeep/ChapterKeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChapterKeepApp.swift
+//  ChapterKeep
+//
+//  Created by 이승윤 on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ChapterKeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChapterKeepTests/ChapterKeepTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChapterKeepTests.swift
+//  ChapterKeepTests
+//
+//  Created by 이승윤 on 9/25/24.
+//
+
+import Testing
+@testable import ChapterKeep
+
+struct ChapterKeepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
