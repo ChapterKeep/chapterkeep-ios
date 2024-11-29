ec482caa46d2d5e2ca89d15940cd4a2480518fa6
Seungyun Lee <khora.lee.0@gmail.com>
2024-11-29 23:11:35 +0900

    feat: add basic stuff for networking

---
+++ ChapterKeep/Model/APIResponse.swift
@@ -0,0 +1,12 @@
+//
+//  APIResponse.swift
+//  ChapterKeep
+//
+//  Created by 이승윤 on 11/29/24.
+//
+
+struct APIResponse<T>: Codable where T: Codable {
+    let code: String
+    let message: String
+    let data: T
+}
